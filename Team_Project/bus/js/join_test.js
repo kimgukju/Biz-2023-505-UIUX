ec0ffef613fc53b25b5c115358ca7ef8e68949e9
kimgukju <kimgukju@kimgukju.com>
2023-06-13 00:45:09 +0900

    js

---
+++ Team_Project/bus/js/join_test.js
@@ -0,0 +1,18 @@
+const INPUT_INDEX = {
+    EMAIL : 0,
+    WRITER : 1,
+    PASSWORD : 2,
+    RE_PASSWORD : 3,
+};
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    const join = () => {
+        const join_inputs = document.querySelectorAll("");
+        if (!join_inputs[INPUT_INDEX.EMAIL].value) {
+            alert("이메일은 필수 항목입니다");
+            join_inputs[INPUT_INDEX.EMAIL].focus();
+            return false;
+          }
+    };
+    document.querySelector("#signUp").addEventListener("click", join);
+});
