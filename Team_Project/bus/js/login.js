223199b1d6767c1bdbb63bdc8341a8fe08b667d3
kimgukju <kimgukju@kimgukju.com>
2023-06-27 14:07:35 +0900

    프로젝트

--- Team_Project/bus/js/login.js
+++ Team_Project/bus/js/login.js
@@ -16,9 +16,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // 조건 다 충족시 + index(메인화면) 이동
     alert("로그인 완료");
+    alert(
+      `입력한 ID : ${user_email.value}
+입력한 PASSWORD : ${user_password.value}`
+    );
     window.location.href = "index.html";
     // window.open("index.html"); 새창에서 열기
   };
+
+  // const login_test = () => {
+  //   const user_email = document.querySelector("input[name='email'] ");
+  //   const user_password = document.querySelector("input[name='password']");
+
+  //   if (!user_email.value == "busTable") {
+  //     alert("아이디를 확인해주세요");
+  //     user_email.focus();
+  //     return false;
+  //   }
+  // };
 
   document.querySelector("#login_button").addEventListener("click", login);
 });
